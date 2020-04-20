65bc543532102e0253fdb7ab317d2a74f9da85d1
Chahyung Lee <leech8757@gmail.com>
2020-04-21 00:06:35 +0900

    Recycler Activity 기본 구조

--- app/src/main/java/com/example/scalecanvasrecycler/RecyclerActivity.kt
+++ app/src/main/java/com/example/scalecanvasrecycler/RecyclerActivity.kt
@@ -1,14 +1,12 @@
 package com.example.scalecanvasrecycler
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class RecyclerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        startActivity(Intent(this, RecyclerActivity::class.java))
+        setContentView(R.layout.activity_recycler)
     }
 }
