d9d4329f8b02708d091e4e8ddeb1c19be9c806a9
Devinear <leech8757@gmail.com>
2020-04-24 12:33:18 +0900

    스크롤의 위 / 아래 동작 추가

--- app/src/main/java/com/example/customcanvas/CanvasScrollActivity.kt
+++ app/src/main/java/com/example/customcanvas/CanvasScrollActivity.kt
@@ -3,16 +3,21 @@ package com.example.customcanvas
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.Paint
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ScrollView
 import androidx.appcompat.app.AppCompatActivity
 
 class CanvasScrollActivity : AppCompatActivity() {
 
+    lateinit var scrollView : ScrollView
     lateinit var canvasView : CanvasView
-    lateinit var btn : Button
+
+    lateinit var btAdd : Button
+    lateinit var btUp : Button
+    lateinit var btDown : Button
+
     private var tempBitmap: Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,31 +25,44 @@ class CanvasScrollActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_canvas_scoll)
 
+        scrollView = findViewById(R.id.scrollView)
         canvasView = findViewById(R.id.canvasView)
-        btn = findViewById(R.id.btn)
-        btn.setOnClickListener { clickBtn() }
+        btAdd = findViewById(R.id.btn_add)
+        btAdd.setOnClickListener { clickAddBitmap() }
+        btUp = findViewById(R.id.btn_up)
+        btUp.setOnClickListener { clickUpScroll() }
+        btDown = findViewById(R.id.btn_down)
+        btDown.setOnClickListener { clickDownScroll() }
 
 //        val bitmap : Bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
 //        val canvas = Canvas(bitmap)
 //        canvas.drawColor(Color.WHITE)
     }
 
-    private fun clickBtn() {
-        Log.d(TAG, "clickBtn")
+    private fun clickAddBitmap() {
+        Log.d(TAG, "clickAddBitmap")
 
-        tempBitmap = Bitmap.createBitmap(500, 1000, Bitmap.Config.ARGB_8888)
+        tempBitmap = Bitmap.createBitmap(900, 1000, Bitmap.Config.ARGB_8888)
         if(tempBitmap != null) {
             val canvas = Canvas(tempBitmap!!)
             canvas.drawColor(Color.RED)
-
-//            val paint = Paint()
-//            paint.color = applicationContext.getColor(R.color.colorPrimaryDark)
-//            canvas.drawText("A", 10f, 10f, paint)
         }
         canvasView.addBitmap(tempBitmap!!)
+//        canvasView.invalidate()
         canvasView.requestLayout()
+
+        scrollView.smoothScrollTo(0, canvasView.height)
     }
 
+    private fun clickUpScroll() {
+        Log.d(TAG, "clickUpScroll")
+        scrollView.smoothScrollTo(0, 0)
+    }
+
+    private fun clickDownScroll() {
+        Log.d(TAG, "clickDownScroll")
+        scrollView.smoothScrollTo(0, canvasView.height)
+    }
 
     companion object {
         const val TAG = "[DE][AC] Activity"
