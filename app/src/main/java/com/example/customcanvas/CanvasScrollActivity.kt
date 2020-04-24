cda9c204e4fbc10ee3c7d9a5d8c3104c6a9f9fda
Devinear <leech8757@gmail.com>
2020-04-24 11:44:33 +0900

    임의의 비트맵을 커스텀 뷰에 추가하는 동작 추가

--- app/src/main/java/com/example/customcanvas/CanvasScrollActivity.kt
+++ app/src/main/java/com/example/customcanvas/CanvasScrollActivity.kt
@@ -1,17 +1,52 @@
 package com.example.customcanvas
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class CanvasScrollActivity : AppCompatActivity() {
 
     lateinit var canvasView : CanvasView
+    lateinit var btn : Button
+    private var tempBitmap: Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_canvas_scoll)
 
         canvasView = findViewById(R.id.canvasView)
+        btn = findViewById(R.id.btn)
+        btn.setOnClickListener { clickBtn() }
+
+//        val bitmap : Bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+//        val canvas = Canvas(bitmap)
+//        canvas.drawColor(Color.WHITE)
     }
 
+    private fun clickBtn() {
+        Log.d(TAG, "clickBtn")
+
+        tempBitmap = Bitmap.createBitmap(500, 1000, Bitmap.Config.ARGB_8888)
+        if(tempBitmap != null) {
+            val canvas = Canvas(tempBitmap!!)
+            canvas.drawColor(Color.RED)
+
+//            val paint = Paint()
+//            paint.color = applicationContext.getColor(R.color.colorPrimaryDark)
+//            canvas.drawText("A", 10f, 10f, paint)
+        }
+        canvasView.addBitmap(tempBitmap!!)
+        canvasView.requestLayout()
+    }
+
+
+    companion object {
+        const val TAG = "[DE][AC] Activity"
+    }
 }
--- app/src/main/java/com/example/customcanvas/CanvasView.kt
+++ app/src/main/java/com/example/customcanvas/CanvasView.kt
@@ -1,8 +1,11 @@
 package com.example.customcanvas
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
@@ -15,16 +18,47 @@ class CanvasView : View {
 
     constructor(context: Context, attr: AttributeSet, defStyleAttr: Int) : super(context, attr, defStyleAttr)
 
+    private val list = mutableListOf<Bitmap>()
+    private var viewHeight : Int = 0
+
+    @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas?) {
-        Log.d(TAG, "onDraw")
-        super.onDraw(canvas)
+//        super.onDraw(canvas)
+        Log.d(TAG, "onDraw count:${list.size}")
+        if(canvas == null) return
+
+        val paint = Paint()
+        paint.color = context.getColor(R.color.colorPrimaryDark)
+        paint.textSize = 50f
+
+
+        var index = 1
+        var height = 0
+        list.forEach {
+            val src = Rect(0, 0, it.width, it.height)
+            val dst = Rect(0, height, it.width, height+it.height)
+//            Log.d(TAG, "onDraw Page:$index Src:$src Dst:$dst")
+            canvas.drawBitmap(it, src, dst, null)
+            canvas.drawText("Page:$index", 10f, (dst.top + 50f), paint)
+
+            index += 1
+            height += 10
+            height += it.height
+            viewHeight = dst.bottom
+        }
     }
 
     fun addBitmap(bitmap: Bitmap) {
-        Log.d(TAG, "onDraw")
+        Log.d(TAG, "addBitmap width:${bitmap.width} height:${bitmap.height}")
+        list.add(bitmap)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        Log.d(TAG, "onMeasure width:$widthMeasureSpec/$width height:$heightMeasureSpec/$height/$viewHeight")
+        setMeasuredDimension(widthMeasureSpec, viewHeight)
     }
 
     companion object {
-        const val TAG = "[VI] Canvas"
+        const val TAG = "[DE][VI] Canvas"
     }
 }
