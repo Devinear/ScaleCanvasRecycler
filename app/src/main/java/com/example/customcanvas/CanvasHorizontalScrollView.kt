a71c560e43d0c443bc61e5a3692effb513a9e794
Devinear <leech8757@gmail.com>
2020-06-24 11:01:48 +0900

    add horizontal scrollview

---
+++ app/src/main/java/com/example/customcanvas/CanvasHorizontalScrollView.kt
@@ -0,0 +1,15 @@
+package com.example.customcanvas
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.HorizontalScrollView
+
+class CanvasHorizontalScrollView : HorizontalScrollView {
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attr: AttributeSet) : super(context, attr)
+    constructor(context: Context, attr: AttributeSet, defStyleAttr: Int) : super(context, attr, defStyleAttr)
+
+
+
+}
